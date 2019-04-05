00a671125161aeb603439713abf2fd4adc2926c3
Nigel Phillips <neastern101@yahoo.com>
2019-04-05 18:08:33 -0500

    frontend code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,72 +5,49 @@ $(document).ready(function() {
     var wins = 0;
     var losses = 0;
 
-    init();
-    playGame();
-    updateStats();
+    
+    $('.toast').toast('hide');
+
+    // $('.toast').toast('show');
 
     function playGame() {
         $('#one').on('click', function () {
-            userTotal = userTotal + num1;
-            console.log("New userTotal= " + userTotal);
-            $('#finalTotal').text(userTotal);
-            //sets win/lose conditions
-            if (userTotal == targetNumber) {
-                updateWins();
-            }
-            else if (userTotal > targetNumber) {
-                udpateLoses();
-            }
+            userTotal = userTotal + num1;       
+            checkGameStatus();
         })
         $('#two').on('click', function () {
             userTotal = userTotal + num2;
-            console.log("New userTotal= " + userTotal);
-            $('#finalTotal').text(userTotal);
-            if (userTotal == targetNumber) {
-                updateWins();
-            }
-            else if (userTotal > targetNumber) {
-                udpateLoses();
-            }
+            checkGameStatus();
         })
         $('#three').on('click', function () {
-            userTotal = userTotal + num3;
-            console.log("New userTotal= " + userTotal);
-            $('#finalTotal').text(userTotal);
-            //sets win/lose conditions
-            if (userTotal == targetNumber) {
-                updateWins();
-            }
-            else if (userTotal > targetNumber) {
-                udpateLoses();
-            }
+            userTotal = userTotal + num3;     
+            checkGameStatus();
         })
         $('#four').on('click', function () {
             userTotal = userTotal + num4;
-            console.log("New userTotal= " + userTotal);
-            $('#finalTotal').text(userTotal);
-
-            if (userTotal == targetNumber) {
-                updateWins();
-            }
-            else if (userTotal > targetNumber) {
-                udpateLoses();
-            }
+            checkGameStatus();
         });
     }
+
 
     function updateStats() {
         $('#randomNumber').text(targetNumber);
         $('#numberWins').text(wins);
         $('#numberLosses').text(losses);
+
+        $('#randomNumber').text(targetNumber);
+        $('#finalTotal').text(userTotal);
     }
+
 
     function generateRandomNumnber(min, max) {
         return Math.floor(Math.random() * max) + min;
     }
 
+
     function init() {
         log("Initialize...");
+        console.clear();
 
         targetNumber = generateRandomNumnber(19, 101);
         userTotal = 0;
@@ -81,28 +58,56 @@ $(document).ready(function() {
             this["num" + i] = generateRandomNumnber(1, 12);
             console.log(this["num" + i]);
         }
-        $('#randomNumber').text(targetNumber);
-        $('#finalTotal').text(userTotal);
-
-
+        
+        updateStats();
     }
 
-    function updateWins() { 
-        alert("You won!");
-        wins++;
-        $('#numberWins').text(wins);
-        init();
-    }
 
-    function udpateLoses() {
-        alert("You lose!");
-        losses++;
-        $('#numberLosses').text(losses);
-        init()
-    }
+    // function updateWins() { 
+    //     alert("You won!");
+    //     wins++;
+    //     $('#numberWins').text(wins);
+    //     init();
+    // }
+
+
+    // function udpateLoses() {
+    //     alert("You lose!");
+    //     losses++;
+    //     $('#numberLosses').text(losses);
+    //     init()
+    // }
+
 
     function log(message) {
         console.log(message);
     }
 
+
+    function checkGameStatus() {
+        console.log("New userTotal= " + userTotal);
+        $('#finalTotal').text(userTotal);
+        if (userTotal == targetNumber) {
+            //updateWins();
+            //alert("You won!");
+            $('.toast').toast('show');
+            wins++;
+            $('#numberWins').text(wins);
+            init();
+            
+        }
+        else if (userTotal > targetNumber) {
+            //udpateLoses();
+            alert("You lose!");
+            losses++;
+            $('#numberLosses').text(losses);
+            init()
+        }
+    }
+
+    init();
+    playGame();
+    updateStats();
 }); 
+
+
